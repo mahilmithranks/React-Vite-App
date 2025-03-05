8a3e8b108b4c91dd14ab87e5e39b120fe7926d90
mahilmithranks <mahilmithranks2007@gmail.com>
2025-03-05 14:46:13 +0530

    Displayed

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Mahil Mithran</h1>
+      <p>Contact: mahil.mithran@example.com</p>
+    </div>
+    </>
+  )
+}
+
+export default App
